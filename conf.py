1eba089b4d872c9c4a7732614c3fac7f773733e0
Blaise Laflamme <blaise@laflamme.org>
2010-11-26 23:12:10 -0500

    Updated docs for new theme

--- conf.py
+++ conf.py
@@ -38,7 +38,7 @@ for item in os.listdir(parent):
 extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['.templates']
+templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -105,15 +105,15 @@ html_theme = 'pylons'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'The Pylons Project Documentation v%s' % release
 
 # A shorter title for the navigation bar.  Default is the same as
 # html_title.
-#html_short_title = None
+html_short_title = 'Home'
 
 # The name of an image file (within the static path) to place at the top of
 # the sidebar.
-# html_logo = '.static/logo_hi.gif'
+html_logo = '_static/pylons.png'
 
 # The name of an image file (within the static path) to use as favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or
@@ -124,7 +124,7 @@ html_theme = 'pylons'
 # here, relative to this directory. They are copied after the builtin
 # static files, so a file named "default.css" will overwrite the builtin
 # "default.css".
-html_static_path = ['.static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
@@ -185,7 +185,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the
 # top of the title page.
-latex_logo = '.static/logo_hi.gif'
+latex_logo = '_static/pylons_small.png'
 
 # For "manual" documents, if this is true, then toplevel headings are
 # parts, not chapters.
