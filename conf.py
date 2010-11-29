f9faa7ec50b2fcf1acb39bbafda78ab2bb34113a
Blaise Laflamme <blaise@laflamme.org>
2010-11-29 15:28:03 -0500

    Updated conf.py

--- conf.py
+++ conf.py
@@ -38,7 +38,7 @@ for item in os.listdir(parent):
 extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -126,7 +126,7 @@ html_favicon = '_static/pylons.ico'
 # here, relative to this directory. They are copied after the builtin
 # static files, so a file named "default.css" will overwrite the builtin
 # "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
@@ -187,7 +187,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the
 # top of the title page.
-latex_logo = '_static/pylons_small.png'
+latex_logo = '_static/pylons-latex.png'
 
 # For "manual" documents, if this is true, then toplevel headings are
 # parts, not chapters.
