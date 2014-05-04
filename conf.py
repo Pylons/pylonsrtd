7430bb1ac2a1b391223ead4a64cac34b8ded16ee
Steve Piercy <web@stevepiercy.com>
2014-05-03 21:09:21 -0700

    - add intersphinx module - add intersphinx mapping for pyramid

--- conf.py
+++ conf.py
@@ -26,7 +26,16 @@ import datetime
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    ]
+
+# Looks for pyramid's objects
+intersphinx_mapping = {
+    'pyramid':
+    ('http://docs.pylonsproject.org/projects/pyramid/en/latest/', None),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['_templates']
